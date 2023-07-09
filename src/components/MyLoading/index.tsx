73de6b899dfef77c89c40c38ebc7c720d6557002
Chin <chin@Trinhs-MacBook-Pro.local>
2023-07-09 16:07:11 +0700

    Feature: handle loadng UI

--- src/components/MyLoading/index.tsx
+++ src/components/MyLoading/index.tsx
@@ -1,7 +1,14 @@
+import { Loader2 } from "lucide-react";
+
 const MyLoading = () => {
   return (
-    <div>MyLoading</div>
-  )
-}
+    <div
+      role="status"
+      className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
+    >
+      <Loader2 className="animate-spin w-8 h-8" aria-hidden="true" />
+    </div>
+  );
+};
 
-export default MyLoading
+export default MyLoading;
